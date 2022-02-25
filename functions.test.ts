9bf45243844e288671a7f130a118603aa10d057f
Keith Mair <mairck@gmail.com>
2022-02-25 12:14:15 -0700

    initial commit

---
+++ functions.test.ts
@@ -0,0 +1,9 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    test('shuffleArray should be an array', () => {
+        expect(typeof(shuffleArray)).toBe(array)
+    })
+
+    test('')
+})
